597fb2ba71ded850b6f212f7dd5d32f6fb3baa07
puscasu <iulian.p234@hotmail.com>
2020-04-23 17:15:29 +0100

    autostart

--- autostart.sh
+++ autostart.sh
@@ -2,19 +2,19 @@
 
 ## exec_always
 feh --bg-fill /home/kyukee/Pictures/wallpapers/imageedit_8_8640256032.png
-setxkbmap pt &
+setxkbmap pt
 xrdb ~/.Xresources
 #/home/kyukee/Scripts/i3_dynamic_window_titles.sh
 
 picom -b
-xflux -l 38.7:-9.1 -k 4500
-http-server ./.startpage -p 9000
-mpd
+xflux -l 38.7:-9.1 -k 4500 &
+http-server ./.startpage -p 9000 &
+mpd &
 dunst -c ~/.config/dunst/dunstrc &
 #nm-applet
 #blueberry-tray
 #volumeicon
 #volti
 #yad --notification --command='gsimplecal' --image='/usr/share/icons/clarity-albus/16x16/apps/calendar.png' --text='yad notification - Calendar'
-nice -n 19 /home/kyukee/Scripts/fmenu-rofi.sh -u
+nice -n 19 /home/kyukee/Scripts/fmenu-rofi.sh -u &
 (sleep 1; urxvt) &
