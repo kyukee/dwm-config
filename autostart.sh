3551543310e34ce958ff0e2df9f998cf8257d4c9
puscasu <iulian.p234@hotmail.com>
2020-04-26 01:02:38 +0100

    enable tray icons

--- autostart.sh
+++ autostart.sh
@@ -17,15 +17,17 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
+# tray icons
 xflux -l 38.7:-9.1 -k 4500 &
 http-server ./.startpage -p 9000 &
 mpd &
 dunst -c ~/.config/dunst/dunstrc &
-#nm-applet
-#blueberry-tray
-#volumeicon
-#volti
-#yad --notification --command='gsimplecal' --image='/usr/share/icons/clarity-albus/16x16/apps/calendar.png' --text='yad notification - Calendar'
+nm-applet &
+blueberry-tray &
+volumeicon &
+volti &
+yad --notification --command='gsimplecal' --image='/usr/share/icons/clarity-albus/16x16/apps/calendar.png' --text='yad notification - Calendar' &
+
 nice -n 19 /home/kyukee/Scripts/fmenu-rofi.sh -u &
 ./dwmbar.sh &
 (sleep 1; urxvt) &
