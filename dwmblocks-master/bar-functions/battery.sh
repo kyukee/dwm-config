dcfc369c182a577a5b28f318e9d6ed3593ac8bbc
puscasu <iulian.p234@hotmail.com>
2020-04-30 17:07:16 +0100

    battery script

--- dwmblocks-master/bar-functions/battery.sh
+++ dwmblocks-master/bar-functions/battery.sh
@@ -6,14 +6,12 @@ battery() {
     icon=""
     charge_icon=""
 
-    if [ "$charge" -eq 1 ]; then
-        charge_icon=" "
-    fi
-
     if [ "$battery" -lt 20 ]; then
-        icon="!! "
+        icon=""
+        charge_icon=" !!"
     elif [ "$battery" -lt 40 ]; then
-        icon="! "
+        icon=""
+        charge_icon=" !"
     elif [ "$battery" -lt 50 ]; then
         icon=""
     elif [ "$battery" -lt 80 ]; then
@@ -22,6 +20,10 @@ battery() {
         icon=""
     else
         icon=""
+    fi
+
+    if [ "$charge" -eq 1 ]; then
+        charge_icon=" "
     fi
 
     printf "%s" "$icon $battery$charge_icon"
