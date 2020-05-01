e05b50b276f6251184d05a908d000e7697825523
puscasu <iulian.p234@hotmail.com>
2020-05-01 23:18:14 +0100

    chenge some colours

--- config.def.h
+++ config.def.h
@@ -32,12 +32,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_red_bright[]  = "#c52f5e";
+static const char col_red_bright[]  = "#BD335E";
 static const char col_red[]         = "#822B45";
 static const char col_purple_light[]= "#564c69";
-// static const char col_purple_dark[] = "#2c0d68";
-static const char col_purple_dark[] = "#482370";
+static const char col_purple_dark[] = "#42265E";
 static const unsigned int baralpha = 0xc3;      /* alpha range: 00 - ff */
+static const unsigned int baralphasel = 0xfc;
 static const unsigned int borderalpha = 0x8f;
 static const char *colors[][4]      = {
 	/*               fg         bg                border            float border*/
@@ -45,9 +45,9 @@ static const char *colors[][4]      = {
 	[SchemeSel]  = { col_gray4, col_purple_dark,  col_red,          col_red_bright },
 };
 static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	/*               fg      bg           border     */
+	[SchemeNorm] = { OPAQUE, baralpha   , borderalpha },
+	[SchemeSel]  = { OPAQUE, baralphasel, borderalpha },
 };
 
 /* terminal emulator */
