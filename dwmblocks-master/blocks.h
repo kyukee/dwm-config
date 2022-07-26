446190e088eceabdb16fe1c8aa5109ada987bab2
Iulian Puscasu <iulian.puscasu.mail@gmail.com>
2022-07-26 16:47:48 +0100

    Update dwmblocks

--- dwmblocks-master/blocks.h
+++ dwmblocks-master/blocks.h
@@ -2,10 +2,6 @@
 static const Block blocks[] = {
     /*Icon*/	        /*Command*/		                        /*Update Interval*/	  /*Update Signal*/
 
-    // {"", "printf 'dwm'",                                                  0,                0},
-
-    // {"", "printf ';'",                                                    0,                0},
-
     {"", "~/Development/dwm/dwmblocks-master/bar-functions/cpu_load.sh",               2,                0},
 
     {"", "~/Development/dwm/dwmblocks-master/bar-functions/cpu_temp.sh",               2,                0},
@@ -19,3 +15,4 @@ static const Block blocks[] = {
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char* delim = "     ";
+static unsigned int delimLen = 5;
